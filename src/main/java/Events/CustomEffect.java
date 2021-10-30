99e8937a33441941eaddfbe79aa408bac72bb0a2
KTAN <ketessar.tan@gmail.com>
2021-10-30 17:30:23 +0200

    Added biome and item pickup events

--- src/main/java/Events/CustomEffect.java
+++ src/main/java/Events/CustomEffect.java
@@ -3,18 +3,56 @@ package Events;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.block.Biome;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityPotionEffectEvent;
+import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.event.player.PlayerMoveEvent;
 
-import java.sql.SQLOutput;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CustomEffect implements Listener {
+
+    private final int pickupCooldown = 60;
+
+    private Map<Entity, Long> entityPickupCooldown = new HashMap<>();
+
+    private final Map<Material, String> specialItemPickups = new HashMap<>(){{
+        put(Material.DIAMOND, "Takusan no kibou to kirameki noseta uta o utaou\nKono daiamondo\n" +
+                "Kono daiamondo hikatte... :cry:");
+        put(Material.EMERALD, "PIKEYYY!");
+    }};
+
+    private final List<Biome> umiBiomes = new ArrayList<>(){{
+        add(Biome.OCEAN);
+        add(Biome.COLD_OCEAN);
+        add(Biome.DEEP_COLD_OCEAN);
+        add(Biome.DEEP_FROZEN_OCEAN);
+        add(Biome.DEEP_OCEAN);
+        add(Biome.LUKEWARM_OCEAN);
+        add(Biome.WARM_OCEAN);
+        add(Biome.FROZEN_OCEAN);
+    }};
+
+    private final List<Biome> evergreenBiomes = new ArrayList<>() {{
+       add(Biome.TAIGA);
+       add(Biome.TAIGA_HILLS);
+       add(Biome.TAIGA_MOUNTAINS);
+       add(Biome.GIANT_SPRUCE_TAIGA);
+       add(Biome.GIANT_TREE_TAIGA);
+       add(Biome.GIANT_SPRUCE_TAIGA_HILLS);
+       add(Biome.GIANT_TREE_TAIGA_HILLS);
+    }};
+
+    private List<Player> umiBiomesPlayers = new ArrayList<>();
+    private List<Player> evergreenPlayers = new ArrayList<>();
 
     private Map<String, Integer> milkDrinkers = new HashMap<>();
     @EventHandler
@@ -35,6 +73,37 @@ public class CustomEffect implements Listener {
                 Bukkit.broadcastMessage(name + " loves milk");
                 this.milkDrinkers.put(name, 0);
             }
+        }
+
+    }
+
+    @EventHandler
+    public void UmiKimochi(PlayerMoveEvent pme) {
+
+        Player p = pme.getPlayer();
+        Biome loc = p.getLocation().getBlock().getBiome();
+
+        if(this.umiBiomes.contains(loc) && !this.umiBiomesPlayers.contains(p)){
+            Bukkit.broadcastMessage(String.format("<%s> 海を見ているといい気分になる。", p.getName()));
+            this.umiBiomesPlayers.add(p);
+        } else if (this.evergreenBiomes.contains(loc) && !this.evergreenPlayers.contains(p)) {
+            Bukkit.broadcastMessage(String.format("<%s> Dokomademo hirogatte iru evaaguriin to sora", p.getName()));
+            this.evergreenPlayers.add(p);
+        }
+    }
+
+    @EventHandler
+    public void itemGet(EntityPickupItemEvent epie) {
+
+        Material item = epie.getItem().getItemStack().getType();
+        Entity e = epie.getEntity();
+
+        if(!specialItemPickups.containsKey(item))
+            return;
+
+        if(!entityPickupCooldown.containsKey(e) || System.currentTimeMillis() > entityPickupCooldown.get(e)) {
+            Bukkit.broadcastMessage(String.format("<%s> %s",e.getName(),specialItemPickups.get(item)));
+            this.entityPickupCooldown.put(e, System.currentTimeMillis() + this.pickupCooldown*1000);
         }
 
     }
